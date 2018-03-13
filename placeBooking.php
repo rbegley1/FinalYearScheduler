0136a18e01cfa5f7ef1cbb03cd3ec3a630fd7df4
Ruairi Begley <ruairibegley@hotmail.co.uk>
2018-03-13 15:21:17 +0000

    Adding fields to booking form

--- placeBooking.php
+++ placeBooking.php
@@ -7,6 +7,10 @@ $dbname = "bookingsdb";
 $clubname = $_POST['clubname'];
 $county = $_POST['county'];
 $address = $_POST['address'];
+$email = $_POST['email'];
+$startdate = $_POST['startdate'];
+$enddate = $_POST['enddate'];
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -15,8 +19,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO bookings (ClubName, County, Address)
-VALUES ('$clubname', '$county', '$address')";
+$sql = "INSERT INTO bookings (ClubName, County, Address, Email, StartDate, EndDate)
+VALUES ('$clubname', '$county', '$address', '$email', '$sdate', '$enddate')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
