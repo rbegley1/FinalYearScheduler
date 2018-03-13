ec464016b56fba12ceb487e5e8246d93bde02e0f
Ruairi Begley <ruairibegley@hotmail.co.uk>
2018-03-13 15:41:01 +0000

    Added fields to the booking form

---
+++ bookings.php
@@ -0,0 +1,21 @@
+<form id='bookings' method="post" action="placeBooking.php">
+  Club Name: <br> <input type="text" name="clubname">
+  <br>
+  County: <br> <input type="text" name="county">
+  <br>
+  Club Address: <br> <input type="text" name="address" placeholder="Enter address here..">
+  <br>
+  Email: <br> <input type="text" name="email">
+  <br>
+  <!-- Draw Type: <br> <input type="radio" name="drawtype" value="local"> Local
+             <input type="radio" name="drawtype" value="grand"> Grand
+  <br> -->
+  Start Date: <br> <input type="date" name="startdate">
+  <br>
+  End Date: <br> <input type="date" name="enddate">
+  <br>
+  <input type="submit" name="submitbutton" value="Place Booking">
+
+</form>
+
+  <!-- Queries -->
