c97fa4165b780a160dc40c7a306ed3e0606531e0
Ruairi Begley <ruairibegley@hotmail.co.uk>
2018-03-16 20:14:29 +0000

    Adding required fields to the booking form.

--- bookings.php
+++ bookings.php
@@ -1,18 +1,18 @@
 <form id='bookings' method="post" action="placeBooking.php">
-  Club Name: <br> <input type="text" name="clubname">
+  Club Name: <br> <input type="text" name="clubname" required>
   <br>
-  County: <br> <input type="text" name="county">
+  County: <br> <input type="text" name="county" required>
   <br>
-  Club Address: <br> <input type="text" name="address" placeholder="Enter address here..">
+  Club Address: <br> <input type="text" name="address" placeholder="Enter address here.." required>
   <br>
-  Email: <br> <input type="text" name="email">
+  Email: <br> <input type="text" name="email" required>
   <br>
-  Draw Type: <br> <input type="radio" name="drawtype" value="local"> Local
-             <input type="radio" name="drawtype" value="grand"> Grand
+  Draw Type: <br> <input type="radio" name="drawtype" value="local" required> Local
+             <input type="radio" name="drawtype" value="grand" required> Grand
   <br>
-  Start Date: <br> <input type="date" name="startdate">
+  Start Date: <br> <input type="date" name="startdate" required>
   <br>
-  End Date: <br> <input type="date" name="enddate">
+  End Date: <br> <input type="date" name="enddate" required>
   <br>
   <input type="submit" name="submitbutton" value="Place Booking">
 
