fe8d2fac61566bd08208329c35eda933a5ff5520
Ruairi Begley <ruairibegley@hotmail.co.uk>
2018-04-25 18:04:58 +0100

    PHP files with comments

--- php/clubsWithActiveDraws.php
+++ php/clubsWithActiveDraws.php
@@ -4,6 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "bookingsdb";
 
+//Data which we will need to GET
 $startdate = $_GET['startdate'];
 $enddate = $_GET['enddate'];
 
@@ -15,17 +16,16 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// run the above queries
-// convert each result to an array of the club IDs
-// if a club id exists in either list, then it has conflicting draw dates
-// run below query to retrieve latitude and longitude of any club that appears in either list
-// $clubsWithConflictingDraws = array();
-// while($row = mysqli_fetch_array($queryOutsideDates, MYSQLI_NUM)){
-//     $clubsWithConflictingDraws[] = intval($row[0]);
-// }
-$clubsWithoutConflictingDrawsQuery = "SELECT draws.ClubID FROM draws JOIN clubs ON clubs.ID = draws.ClubID WHERE '$enddate' < draws.StartDate OR draws.EndDate < '$startdate'";
-$clubsWithConflictingDrawsQuery = "SELECT ClubName, Latitude, Longitude FROM clubs WHERE ID NOT IN ($clubsWithoutConflictingDrawsQuery)";
+//Will select those clubs which do not have conflicting draws
+$clubsWithoutConflictingDrawsQuery = "SELECT draws.ClubID
+                                      FROM draws JOIN clubs ON clubs.ID = draws.ClubID
+                                      WHERE '$enddate' < draws.StartDate OR draws.EndDate < '$startdate'";
+//If a club is not in the first query they will have a conflicting draw, those clubs will be in this query
+$clubsWithConflictingDrawsQuery = "SELECT ClubName, Latitude, Longitude
+                                   FROM clubs
+                                   WHERE ID NOT IN ($clubsWithoutConflictingDrawsQuery)";
 
+//These clubs will be added to an array
 $clubsWithConflictingDraws = array();
 if ($result = mysqli_query($conn, $clubsWithConflictingDrawsQuery)) {
     $tempArray = array();
@@ -34,10 +34,12 @@ if ($result = mysqli_query($conn, $clubsWithConflictingDrawsQuery)) {
         $tempArray = $row;
         array_push($clubsWithConflictingDraws, $tempArray);
     }
-
+    //Will change the SQL list from the query to JSON code
     header('Content-Type: application/json');
     echo json_encode($clubsWithConflictingDraws);
 }
 
+//Close connection
 mysqli_close($conn);
+
 ?>
--- php/placeBooking.php
+++ php/placeBooking.php
@@ -4,6 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "bookingsdb";
 
+//Variables to POST
 $clubname = $_POST['clubname'];
 $address = $_POST['address'];
 $drawtype = $_POST['drawtype'];
@@ -20,9 +21,11 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+//Check for existing club names in clubs table
 $result = mysqli_query($conn, "SELECT * FROM clubs WHERE ClubName='$clubname'");
 $rowcount = mysqli_num_rows($result);
 
+//If greater than 0, record will be updated, else it will be inserted to the clubs table
 if($rowcount > 0) {
   mysqli_query($conn, "UPDATE clubs SET Address= '$address', Latitude= '$latitude', Longitude= '$longitude' WHERE ClubName='$clubname'");
   echo "The record has been updated";
@@ -36,82 +39,7 @@ else {
  $sql = mysqli_query($conn,"INSERT INTO draws (ClubID, DrawType, StartDate, EndDate)
  VALUES ('$conn->insert_id', '$drawtype', '$startdate', '$enddate')");
 
-
-
-// $isError = hasConflictingClubs($conn, $latitude, $longitude);
-// if ($isError) {
-//   echo "There was an error.";
-// }
-
 //Close connection
-$conn->close();
+mysqli_close($conn);
 
-// function hasConflictingClubs($connection, $lat, $long) {
-  // use dates to query draws table
-  // use club info associated with active draws
-  // get lat and long of each of these clubs
-  // use $latitude and $longitude as origin
-  // use draw clubs lat and longs as destination
-  // make request to matrix api
-  // filter results where distance < 20 miles
-  // if results.length > 0, then conflicting draw is present
-  // state name of club(s) that are already hosting draws
-
-  // $distanceMatrixApiUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&key=AIzaSyA5kdoVwkdSvWyl30bYYROScoIVXQfy5d0&origins=$lat,$long&destinations=";
-  // $clubsWithActiveDrawsQuery = "SELECT Latitude, Longitude FROM clubs JOIN draws ON clubs.ID = draws.ClubID";
-  // $clubsWithActiveDrawsResult = mysqli_query($connection, $clubsWithActiveDrawsQuery);
-
-  //Build URL
-   // if (mysqli_num_rows($clubsWithActiveDrawsResult) > 0) {
-   //  while ($row = mysqli_fetch_assoc($clubsWithActiveDrawsResult)) {
-   //     $distanceMatrixApiUrl = $distanceMatrixApiUrl . $row['Latitude'] . "%2C" . $row['Longitude'] . "%7C";
-   //   }
-   //   print $distanceMatrixApiUrl;
-   //
-   //   $distanceMatrixApiUrl = substr($distanceMatrixApiUrl, 0, strlen($distanceMatrixApiUrl) - 3);
-
-
- //     //Distance between origin and destination
- //     $response = file_get_contents($distanceMatrixApiUrl));
- //
- //
- //     $allDestinations = response.rows[0].elements;
- //
- //     $indexesOfBadClubs = [];
- //
- //     for ($i = 0; $i < $allDestinations.length; $i++) {
- //            var currentDestination = $allDestinations[$i];
- //
- //            if ($currentDestination.distance.value < 20000) {
- //                $indexesOfBadClubs.push[$i]);
- //                break;
- //            }
- //     }
- //
- //     if ($indexesOfBadClubs.length > 0) {
- //       return true;
- //     } else {
- //       return false;
- //     }
- //   }
- //
- //   return false;
- // }
-
-// //Part 1
-// function buildUrl($lat, $long, $clubsWithActiveDraws) {
-// 	$apiUrl = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&key=AIzaSyA5kdoVwkdSvWyl30bYYROScoIVXQfy5d0&origins=${lat},${long}&destinations=`;
-//
-// 	for ($i = 0; $i < count($clubsWithActiveDraws); $i++) {
-//     	$currentClub = $clubsWithActiveDraws[$i];
-//
-// 		$apiUrl += $currentClub.Latitude + '%2C' + $currentClub.Longitude + '%7C';
-// 	}
-//
-// 	return $apiUrl.substring(0, count($apiUrl) - 3);
-// }
-
-//If draw dates already in table check distance
-//If distance inside boundary, error
-//If distance outside boundary, no problems.
 ?>
